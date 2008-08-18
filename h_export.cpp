4051d8b142ff923efaf795cb75cdc28384e2c90a
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2008-08-18 23:54:18 +0300

    workaround gcc4.x 'local static variable' guarding (hlds is single thread so this is ok)

--- h_export.cpp
+++ h_export.cpp
@@ -37,3 +37,32 @@ void WINAPI GiveFnptrsToDll( enginefuncs_t* pengfuncsFromEngine, globalvars_t *p
    gpGlobals = pGlobals;
 }
 
+#ifdef __GNUC__
+/* workaround gcc4.x 'local static variable' guarding (hlds is single thread so this is ok) */
+namespace __cxxabiv1 
+{
+	/* guard variables */
+
+	/* The ABI requires a 64-bit type.  */
+	__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+	extern "C" int __cxa_guard_acquire (__guard *);
+	extern "C" void __cxa_guard_release (__guard *);
+	extern "C" void __cxa_guard_abort (__guard *);
+
+	extern "C" int __cxa_guard_acquire (__guard *g) 
+	{
+		return !*(char *)(g);
+	}
+
+	extern "C" void __cxa_guard_release (__guard *g)
+	{
+		*(char *)g = 1;
+	}
+
+	extern "C" void __cxa_guard_abort (__guard *)
+	{
+	}
+}
+#endif
+
