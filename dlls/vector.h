8fa5ef1073d5cfa25d3ac1b5816b92c41665c75f
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2008-08-24 15:02:33 +0300

    vector.h cleanups

--- dlls/vector.h
+++ dlls/vector.h
@@ -24,19 +24,19 @@ class Vector;
 class Vector2D
 {
 public:
-        inline Vector2D(void): x(0.0f), y(0.0f) { }
+        inline Vector2D(void): x(0.0), y(0.0) { }
         inline Vector2D(float X, float Y): x(X), y(Y) { }
         inline Vector2D operator+(const Vector2D& v)    const   { return Vector2D(x+v.x, y+v.y);        }
         inline Vector2D operator-(const Vector2D& v)    const   { return Vector2D(x-v.x, y-v.y);        }
-        inline Vector2D operator*(float fl)                             const   { return Vector2D(x*fl, y*fl);  }
-        inline Vector2D operator/(float fl)                             const   { return Vector2D(x/fl, y/fl);  }
+        inline Vector2D operator*(float fl)             const   { return Vector2D(x*fl, y*fl);  }
+        inline Vector2D operator/(float fl)             const   { return Vector2D(x/fl, y/fl);  }
         
-        inline float Length(void)                                               const   { return (float)sqrt(x*x + y*y );              }
+        inline double Length(void)                      const   { return sqrt(x*x + y*y );              }
 
         inline Vector2D Normalize ( void ) const
         {
-                float flLen = Length();
-                if ( flLen == 0 )
+                double flLen = Length();
+                if ( (float)flLen == 0.0f )
                         return Vector2D( 0, 0 );
                 else
                         return Vector2D( x / flLen, y / flLen );
@@ -55,29 +55,29 @@ class Vector                                            // same data-layout as e
 {                                                               //              which is a vec_t[3]
 public:
         // Construction/destruction
-        inline Vector(void): x(0.0f), y(0.0f), z(0.0f) { }
+        inline Vector(void): x(0.0), y(0.0), z(0.0) { }
         inline Vector(float X, float Y, float Z): x(X), y(Y), z(Z) { }
         inline Vector(const Vector& v): x(v.x), y(v.y), z(v.z) { } 
         inline Vector(const float rgfl[3]): x(rgfl[0]), y(rgfl[1]), z(rgfl[2]) { }
-        inline Vector(const Vector2D& v): x(v.x), y(v.y), z(0.0f) { }
+        inline Vector(const Vector2D& v): x(v.x), y(v.y), z(0.0) { }
 
         // Operators
-        inline Vector operator-(void) const                     { return Vector(-x,-y,-z);                              }
+        inline Vector operator-(void) const                     { return Vector(-x,-y,-z);              }
         inline int operator==(const Vector& v) const            { return x==v.x && y==v.y && z==v.z;    }
-        inline int operator!=(const Vector& v) const            { return !(*this==v);                                   }
+        inline int operator!=(const Vector& v) const            { return !(*this==v);                   }
         inline Vector operator+(const Vector& v) const          { return Vector(x+v.x, y+v.y, z+v.z);   }
         inline Vector operator-(const Vector& v) const          { return Vector(x-v.x, y-v.y, z-v.z);   }
-        inline Vector operator*(float fl) const                 { return Vector(x*fl, y*fl, z*fl);              }
-        inline Vector operator/(float fl) const                 { fl=1/fl; return Vector(x*fl, y*fl, z*fl);              }
-        inline Vector& operator=(const Vector& v) 		{ x=v.x; y=v.y; z=v.z; return *this; }
+        inline Vector operator*(float fl) const                 { return Vector(x*fl, y*fl, z*fl);      }
+        inline Vector operator/(float _fl) const                { double fl = 1.0 / _fl; return Vector(x*fl, y*fl, z*fl); }
+        inline Vector& operator=(const Vector& v) 		{ x=v.x; y=v.y; z=v.z; return *this;    }
         inline Vector& operator+=(const Vector& v) 		{ x+=v.x; y+=v.y; z+=v.z; return *this; }
         inline Vector& operator-=(const Vector& v) 		{ x-=v.x; y-=v.y; z-=v.z; return *this; }
-        inline Vector& operator*=(float fl) 			{ x*=fl; y*=fl; z*=fl; return *this; }
+        inline Vector& operator*=(float fl) 			{ x*=fl; y*=fl; z*=fl; return *this;    }
         inline Vector& operator/=(float _fl) 			{ double fl = 1.0 / _fl; x*=fl; y*=fl; z*=fl; return *this; }
         
         // Methods
-        inline void CopyToArray(float* rgfl) const              { rgfl[0] = x, rgfl[1] = y, rgfl[2] = z; }
-        inline float Length(void) const                         { return (float)sqrt(x*x + y*y + z*z); }
+        inline void CopyToArray(float* rgfl) const              { rgfl[0] = x; rgfl[1] = y; rgfl[2] = z; }
+        inline double Length(void) const                        { return sqrt(x*x + y*y + z*z);         }
         inline operator float *()                               { return &x; } // Vectors will now automatically convert to float * when needed
         inline operator Vector2D () const                       { return (*this).Make2D(); }
 
@@ -88,7 +88,7 @@ public:
         inline Vector Normalize(void) const
         {
                 double flLen = Length();
-                if (flLen == 0) return Vector(0, 0, 1); // ????
+                if ((float)flLen == 0.0f) return Vector(0, 0, 1); // ????
                 flLen = 1.0 / flLen;
                 return Vector(x * flLen, y * flLen, z * flLen);
         }
@@ -97,15 +97,15 @@ public:
         {
                 return Vector2D(x,y);
         }
-        inline float Length2D(void) const                       { return (float)sqrt(x*x + y*y); }
+        inline double Length2D(void) const                       { return sqrt(x*x + y*y); }
 
         // Members
         vec_t x, y, z;
 };
+
 inline Vector operator*(float fl, const Vector& v)      { return v * fl; }
 inline float DotProduct(const Vector& a, const Vector& b) { return(a.x*b.x+a.y*b.y+a.z*b.z); }
 inline Vector CrossProduct(const Vector& a, const Vector& b) { return Vector( a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x ); }
 
-
-
 #endif
+
