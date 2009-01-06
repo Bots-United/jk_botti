c615f3a40ac551d420454a12e5378187fc9e7cce
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2009-01-07 00:36:19 +0200

    fix 'fix compile on new linux'

--- bot_query_hook_linux.cpp
+++ bot_query_hook_linux.cpp
@@ -21,7 +21,7 @@
 #include "bot_query_hook.h"
 
 #define PAGE_SHIFT      12
-#define PAGE_SIZE       (_AC(1,UL) << PAGE_SHIFT)
+#define PAGE_SIZE       (1UL << PAGE_SHIFT)
 #define PAGE_MASK       (~(PAGE_SIZE-1))
 
 //
